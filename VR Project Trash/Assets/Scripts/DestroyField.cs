17f20930c9b890ab45117052b32b392a1a741c66
Kai Messerschmidt <mess8881@csumb.ad.csumb.edu>
2019-09-27 16:48:11 -0700

    Add conveyer belt Add destroyfield Add assets + matertials = scripts to project

---
+++ VR Project Trash/Assets/Scripts/DestroyField.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyField : MonoBehaviour
+{
+    public string trashName1;
+    public string trashName2;
+    public string trashName3;
+    void Start()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == trashName1)
+        {
+            Destroy(other.gameObject);
+        }
+        else if(other.tag == trashName2){
+            Destroy(other.gameObject);
+        }
+        else if (other.tag == trashName3){
+            Destroy(other.gameObject);
+        }
+    }
+    
+}
---
+++ VR Project Trash/Assets/Scripts/TrashAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TrashAI : MonoBehaviour
+{
+    public Transform endPoint;
+    public NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = this.gameObject.GetComponent<NavMeshAgent>();
+        endPoint = GameObject.Find("Endpoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(endPoint.position);
+    }
+}
