2d31872c45e3a69fc3572f93438e82d7b07c7de9
Jose Perez <pere9100@csumb.ad.csumb.edu>
2019-10-03 21:58:30 -0700

    title screen update

---
+++ VR Project Trash/Assets/Scripts/confetti.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class confetti : MonoBehaviour
+{
+    public GameObject particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("I triggered");
+        particles.GetComponent<ParticleSystem>().Play();
+    }
+}
