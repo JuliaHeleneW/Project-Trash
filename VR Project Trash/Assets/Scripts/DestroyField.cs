57c9935b549d738301d56ab3345860ab38c39b91
Lewis Truong <letruong@csumb.edu>
2019-10-05 00:05:52 -0700

    Scoring System
    Added a scoring system to the filer/destroy scripts. They utilize a Scriptable Object named "Data" to keep track of the player score.
    (this object can be foun in the "Score Object" folder)

--- VR Project Trash/Assets/Scripts/DestroyField.cs
+++ VR Project Trash/Assets/Scripts/DestroyField.cs
@@ -3,24 +3,30 @@ using System.Collections;
 
 public class DestroyField : MonoBehaviour
 {
+    public Score score; //The score object
+    public int takePointAmount;  //The points to deduct when the player misses a toss
+
     public string trashName1;
     public string trashName2;
     public string trashName3;
     void Start()
     {
-
+        score.PlayerScore = 0;          //Reset the score at the start of the game
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == trashName1)
         {
             Destroy(other.gameObject);
+            score.PlayerScore = score.PlayerScore - takePointAmount;    //Deduct the proper amount of points
         }
         else if(other.tag == trashName2){
             Destroy(other.gameObject);
+            score.PlayerScore = score.PlayerScore - takePointAmount;    //Deduct the proper amount of points
         }
         else if (other.tag == trashName3){
             Destroy(other.gameObject);
+            score.PlayerScore = score.PlayerScore - takePointAmount;    //Deduct the proper amount of points
         }
     }
     
