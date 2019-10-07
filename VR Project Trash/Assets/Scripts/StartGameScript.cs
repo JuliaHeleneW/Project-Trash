23ab742afa057daae6f6a694be2f745ff79aed95
Eric Gonzalez Olivares <gonz9019@csumb.ad.csumb.edu>
2019-10-07 13:55:37 -0700

    Added sounds and started title screen functionality

---
+++ VR Project Trash/Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour
+{
+    public Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(delegate { startGame(); });
+    }
+
+    public void startGame()
+    {
+        print("start button clicked");
+        SceneManager.LoadScene("SetupTest");
+    }
+
+}
