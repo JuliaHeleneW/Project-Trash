9e168da85d43664c41152d9a483edbf0f2fd5551
Lewis Truong <letruong@csumb.edu>
2019-10-11 15:55:07 -0700

    Added Random Trash Spawner, Adjusted Placement of Files and Added Components to Garbage

--- VR Project Trash/Assets/Scripts/CreateTrash.cs
+++ VR Project Trash/Assets/Scripts/CreateTrash.cs
@@ -5,7 +5,15 @@ using UnityEngine.AI;
 
 public class CreateTrash : MonoBehaviour
 {
-    public GameObject instantTrash;
+    //Code for making a list of trash and
+    //instantiating random objects from it
+    //was taken from User glasstongue on the Unity
+    //Forums: "Adding prefabs to a list or an array from a folder and instantiating them."
+
+
+    //define a list
+    public static List<GameObject> myListObjects = new List<GameObject>();
+
     public Transform dumper;
     public float cooldown;
     private float cooldownRef;
@@ -15,6 +23,15 @@ public class CreateTrash : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Make a list using all Game objects in the "Trash Objects" folder
+        Object[] subListObjects = Resources.LoadAll("Trash Objects", typeof(GameObject));  
+        
+        foreach (GameObject subListObject in subListObjects)
+        {
+            GameObject lo = (GameObject)subListObject;
+
+            myListObjects.Add(lo);
+        }
         cooldownRef = cooldown;
     }
 
@@ -23,10 +40,28 @@ public class CreateTrash : MonoBehaviour
     {
         if (Time.time > cooldownRef)
         {
+            Debug.Log("Spawning...");
+            SpawnRandomObject();
+
+            /*
             cooldownRef = Time.time + cooldown;
             Instantiate(instantTrash, dumper);
             instantTrash.GetComponent<TrashAI>().SetEndpoint(endpoint);
-            instantTrash.GetComponent<TrashAI>().SetNavAgent(navAgent);
+            instantTrash.GetComponent<TrashAI>().SetNavAgent(navAgent);*/
         }
+    }
+
+    void SpawnRandomObject()
+    {
+        //spawns item in array position between 0 and 100
+        int whichItem = Random.Range(0, myListObjects.Count);
+
+        GameObject trash = Instantiate(myListObjects[whichItem]) as GameObject;
+        cooldownRef = Time.time + cooldown;
+
+        trash.transform.position = dumper.position;
+        trash.GetComponent<TrashAI>().SetEndpoint(endpoint);
+        trash.GetComponent<TrashAI>().SetNavAgent(navAgent);
+
     }
 }
