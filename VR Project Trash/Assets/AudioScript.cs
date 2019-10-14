f3a687d7c72efa73b41d0c51470e6c2b061341b2
Eric Gonzalez Olivares <gonz9019@csumb.ad.csumb.edu>
2019-10-14 13:52:19 -0700

    added first title scene and music plays across scenes

---
+++ VR Project Trash/Assets/AudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    private static AudioScript instance = null;
+    public static AudioScript Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ VR Project Trash/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartScene());
+    }
+
+    IEnumerator StartScene()
+    {
+        yield return new WaitForSeconds(8);
+        LoadScene();
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
