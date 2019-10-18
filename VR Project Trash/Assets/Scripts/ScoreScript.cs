672536666c9e38076dd2d5c639735816f4de7aea
Eric <gonzerc526@gmail.com>
2019-10-18 16:26:29 -0700

    added more models and score updates

---
+++ VR Project Trash/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text Score;
+    public Score s;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Score.text = "Score: " + s.PlayerScore;
+    }
+}
